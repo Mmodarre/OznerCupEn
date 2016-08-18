7ba8065dcdd221993784febca80f36b9676e66ab
taoran <taoran1025@163.com>
2016-08-18 11:51:19 +0800

    修改Alay获取设备型号

--- OznerLibrary/src/main/java/com/ozner/device/OznerDevice.java
+++ OznerLibrary/src/main/java/com/ozner/device/OznerDevice.java
@@ -19,13 +19,13 @@ public abstract class OznerDevice extends XObject {
     private DeviceSetting setting;
     private String Type;
     private String appdata;
+
     public OznerDevice(Context context, String Address, String Type, String Setting) {
         super(context);
         this.address = Address;
         this.Type = Type;
         this.setting = initSetting(Setting);
-        if (Helper.StringIsNullOrEmpty(setting.name()))
-        {
+        if (Helper.StringIsNullOrEmpty(setting.name())) {
             setting.name(getDefaultName());
         }
     }
@@ -35,8 +35,7 @@ public abstract class OznerDevice extends XObject {
      */
     public final static String ACTION_DEVICE_UPDATE = "com.ozner.device.update";
 
-    protected void doUpdate()
-    {
+    protected void doUpdate() {
         Intent intent = new Intent(ACTION_DEVICE_UPDATE);
         intent.putExtra(Extra_Address, Address());
         context().sendBroadcast(intent);
@@ -99,24 +98,24 @@ public abstract class OznerDevice extends XObject {
     protected DeviceSetting initSetting(String Setting) {
         DeviceSetting setting = new DeviceSetting();
         setting.load(Setting);
-        if (Helper.StringIsNullOrEmpty(setting.name()))
-        {
+        if (Helper.StringIsNullOrEmpty(setting.name())) {
             setting.name(getDefaultName());
         }
         return setting;
     }
+
     /**
      * 通知设备将设置存储
      */
-    public void saveSettings()
-    {
+    public void saveSettings() {
 
     }
+
     /**
      * 通知设备设置变更
      */
     public void updateSettings() {
-        OznerDeviceManager.Instance().setDeviceSetting(this.address,this.setting.toString());
+        OznerDeviceManager.Instance().setDeviceSetting(this.address, this.setting.toString());
     }
 
 
@@ -124,34 +123,32 @@ public abstract class OznerDevice extends XObject {
 
     /**
      * 在后台模式时判断接口是否包含有效数据,如果是则连接,否不进行连接
+     *
      * @param io 接口IO
      * @return true包含数据
      */
-    protected boolean doCheckAvailable(BaseDeviceIO io)
-    {
+    protected boolean doCheckAvailable(BaseDeviceIO io) {
         return true;
     }
+
     public boolean Bind(BaseDeviceIO deviceIO) throws DeviceNotReadyException {
 
-        if ((deviceIO!=null) && (!deviceIO.getClass().equals(getIOType())))
-        {
+        if ((deviceIO != null) && (!deviceIO.getClass().equals(getIOType()))) {
             throw new ClassCastException();
         }
 
         if (this.deviceIO == deviceIO)
             return false;
 
-        if ((getRunningMode()==RunningMode.Background) && (deviceIO!=null))
-        {
+        if ((getRunningMode() == RunningMode.Background) && (deviceIO != null)) {
             if (!doCheckAvailable(deviceIO)) return false;
         }
 
-        BaseDeviceIO old=this.deviceIO;
+        BaseDeviceIO old = this.deviceIO;
 
         try {
             doSetDeviceIO(old, deviceIO);
-        }catch (Exception e)
-        {
+        } catch (Exception e) {
 
         }
 
@@ -159,37 +156,42 @@ public abstract class OznerDevice extends XObject {
             this.deviceIO = null;
         }
 
-        this.deviceIO=deviceIO;
+        this.deviceIO = deviceIO;
 
 
         if (deviceIO != null) {
             deviceIO.open();
         }
 
+        if (deviceIO.isReady()) {
+            deviceIO.reCallDoReadly();
+        }
+
+
         return true;
     }
+
     /**
      * 获取设备数据
-     * */
-    public String getDeviceAppdata()
-    {
+     */
+    public String getDeviceAppdata() {
         return OznerDeviceManager.Instance().getDeviceAppData(this.address);
     }
+
     /*
     *
     * */
-    private void setDeviceAppData(String json)
-    {
-        OznerDeviceManager.Instance().setDeviceAppData(this.address,json);
+    private void setDeviceAppData(String json) {
+        OznerDeviceManager.Instance().setDeviceAppData(this.address, json);
     }
+
     /**
      * @Key 获取和APP绑定的字段
-     * */
-    public Object getAppValue(String Key)
-    {
-        String json=getDeviceAppdata();
-        JSONObject jsonObject=null;
-        if(json!=null&&json.length()>0) {
+     */
+    public Object getAppValue(String Key) {
+        String json = getDeviceAppdata();
+        JSONObject jsonObject = null;
+        if (json != null && json.length() > 0) {
             try {
                 jsonObject = new JSONObject(json);
                 return jsonObject.get(Key);
@@ -198,13 +200,13 @@ public abstract class OznerDevice extends XObject {
                 jsonObject = null;
             }
         }
-       return null;
+        return null;
     }
-    public void setAppdata(String Key,Object value)
-    {
-        String json=getDeviceAppdata();
-        JSONObject jsonObject=null;
-        if(json!=null&&json.length()>0) {
+
+    public void setAppdata(String Key, Object value) {
+        String json = getDeviceAppdata();
+        JSONObject jsonObject = null;
+        if (json != null && json.length() > 0) {
             try {
                 jsonObject = new JSONObject(json);
             } catch (Exception ex) {
@@ -212,18 +214,15 @@ public abstract class OznerDevice extends XObject {
                 jsonObject = null;
             }
         }
-        if(jsonObject==null)
-        {
-            jsonObject=new JSONObject();
+        if (jsonObject == null) {
+            jsonObject = new JSONObject();
         }
-        try
-        {
-            jsonObject.put(Key,value);
-        }catch (Exception ex)
-        {
+        try {
+            jsonObject.put(Key, value);
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
-        OznerDeviceManager.Instance().setDeviceAppData(this.address,jsonObject.toString());
+        OznerDeviceManager.Instance().setDeviceAppData(this.address, jsonObject.toString());
     }
 
 }
