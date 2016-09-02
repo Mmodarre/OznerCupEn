04a70aa031d427e3107643416f1496d7673807d9
QX-Zach <767231470@qq.com>
2016-09-02 17:42:19 +0800

    更新欢迎页图片，个人中心设备

--- app/src/main/java/com/ozner/yiquan/mycenter/MyFragment.java
+++ app/src/main/java/com/ozner/yiquan/mycenter/MyFragment.java
@@ -73,8 +73,6 @@ public class MyFragment extends BaseFragment implements View.OnClickListener, Fo
 
     @Override
     public void onDetach() {
-        Log.e(TAG, "onDetach: ");
-        //你好
         super.onDetach();
     }
 
